ffa2bc8c53be9f58925f426b2b52bc83e970fc1a
lhz <pepitsizous@zoho.com>
2021-02-15 16:56:37 +0200

    technically works

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # gommenter
 comment microservice written in go
+
+## env vars
+- `PG_URL` for postgres URL
+- `THANK_PAGE` for thank you page
--- main.go
+++ main.go
@@ -8,72 +8,70 @@ import (
 	"os"
 )
 
-type Comment struct {
-	Name    string `json:"name"`
-	Content string `json:"content"`
-}
-
 const createtable = `CREATE TABLE IF NOT EXISTS comments (name TEXT, content TEXT);`
 
-var pgClient *sql.DB
-
-func newPostgresClient(postgresURL string) (*sql.DB, error) {
-	// create postgres client
-	client, err := sql.Open("postgres", postgresURL)
-	if err != nil {
-		return nil, err
-	}
-	err = client.Ping()
-	if err != nil {
-		return nil, err
-	}
-	_, err = client.Exec(createtable)
-	if err != nil {
-		return nil, err
-	}
-	return client, nil
-}
-
 func addComment(w http.ResponseWriter, r *http.Request) {
+	// this db stuff is here because otherwise there is a nil pointer dereference
+	// unfortunate but such is life
+	postgresURL := os.Getenv("PG_URL")
+	if postgresURL == "" {
+		postgresURL = "postgres://tester:Apasswd@localhost:5432?sslmode=disable"
+	}
+	pgClient, err := sql.Open("postgres", postgresURL)
+	if err != nil {
+		log.Println("error open", err)
+	}
+	err = pgClient.Ping()
+	if err != nil {
+		log.Fatal("error ping", err)
+	}
+	// create database if it doesn't exist
+	_, err = pgClient.Exec(createtable)
+	if err != nil {
+		log.Println("error exec", err)
+	}
+
+	// actually handle request
+	// but only if method is post
 	if r.Method == "POST" {
-		var formErrors = []string{}
 		var name string
 		var content string
-		r.ParseForm()
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatal("error parseform", err)
+			return
+		}
 		if (r.Form["name"] != nil) && (len(r.Form["name"][0]) > 0) {
 			name = r.Form["name"][0]
 		} else {
-			formErrors = append(formErrors, "imma need ya name, sweaty")
+			name = "anon"
 		}
 		if r.Form["content"] != nil {
 			content = r.Form["content"][0]
 		} else {
-			formErrors = append(formErrors, "so you have nothing to say?")
-		}
-		// TODO: make this error handling better
-		if len(formErrors) > 0 {
 			return
 		}
-		// save comment to database if there are no errors
-		query := `INSERT INTO comments (name, content) VALUES ($1, $2);`
-		_, err := pgClient.Exec(query, name, content)
-		// TODO: make this error handling better
+		// save comment to database
+		query := `INSERT INTO comments (name, content) VALUES ($1, $2)`
+		_, err = pgClient.Exec(query, name, content)
 		if err != nil {
 			return
 		}
 		// make a redirect to r.URL.Host contact thank you page
-		// http.Redirect(w, r, r.URL.Host, 301)
+		thankPage := os.Getenv("THANK_PAGE")
+		if thankPage == "" {
+			thankPage = "http://localhost:1313/thanks"
+		}
+		http.Redirect(w, r, thankPage, 301)
 	}
 	return
 }
 
 func main() {
-	pgURL := os.Getenv("POSTGRES_URL")
-	pgClient, err := newPostgresClient(pgURL)
-	if err != nil {
-		log.Println("pgClient", pgClient)
-		log.Fatal("error connecting to postgres", err)
-	}
 	http.HandleFunc("/api/form/new", addComment)
-	http.ListenAndServe(":9097", nil)
+	err := http.ListenAndServe(":9097", nil)
+	if err != nil {
+		log.Fatal("error starting http server", err)
+		return
+	}
 }
