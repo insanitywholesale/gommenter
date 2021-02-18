af41a39d74e3bfaeadca91283e69858d470e71f2
lhz <pepitsizous@zoho.com>
2021-02-18 12:27:13 +0200

    return of the global vars

--- main.go
+++ main.go
@@ -10,29 +10,29 @@ import (
 
 const createtable = `CREATE TABLE IF NOT EXISTS comments (name TEXT, content TEXT);`
 
-func addComment(w http.ResponseWriter, r *http.Request) {
-	// this db stuff is here because otherwise there is a nil pointer dereference
-	// unfortunate but such is life
-	postgresURL := os.Getenv("PG_URL")
-	if postgresURL == "" {
-		postgresURL = "postgres://tester:Apasswd@localhost:5432?sslmode=disable"
-	}
-	pgClient, err := sql.Open("postgres", postgresURL)
-	if err != nil {
-		log.Println("error open", err)
-	}
-	err = pgClient.Ping()
-	if err != nil {
-		log.Fatal("error ping", err)
-	}
-	// create database if it doesn't exist
-	_, err = pgClient.Exec(createtable)
-	if err != nil {
-		log.Println("error exec", err)
-	}
+var (
+	pgClient *sql.DB
+	thankPage string
+)
 
-	// actually handle request
-	// but only if method is post
+func newPostgresClient(postgresURL string) (*sql.DB, error) {
+	// create postgres client
+	client, err := sql.Open("postgres", postgresURL)
+	if err != nil {
+		return nil, err
+	}
+	err = client.Ping()
+	if err != nil {
+		return nil, err
+	}
+	_, err = client.Exec(createtable)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+func addComment(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		var name string
 		var content string
@@ -57,20 +57,33 @@ func addComment(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			return
 		}
-		// make a redirect to r.URL.Host contact thank you page
-		thankPage := os.Getenv("THANK_PAGE")
-		if thankPage == "" {
-			thankPage = "http://localhost:1313/thanks"
-		}
+		// redirect to thank you page
+		log.Println("thankpage", thankPage)
 		http.Redirect(w, r, thankPage, 301)
-		return
 	}
 	return
 }
 
 func main() {
+	pgURL := os.Getenv("PG_URL")
+	if pgURL == "" {
+		pgURL = "postgresql://tester:Apasswd@localhost:5432/?sslmode=disable"
+	}
+	client, err := newPostgresClient(pgURL)
+	pgClient = client
+	if err != nil {
+		log.Fatal("error connecting to postgres", err)
+	}
+	thankPage = os.Getenv("THANK_PAGE")
+	if thankPage == "" {
+		thankPage = "http://localhost:1313/thanks"
+	}
 	http.HandleFunc("/api/form/new", addComment)
-	err := http.ListenAndServe(":9097", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9097"
+	}
+	err = http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal("error starting http server", err)
 		return
